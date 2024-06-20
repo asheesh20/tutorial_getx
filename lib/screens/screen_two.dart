516b524aa1219694b527b0b42f76396b411e5f0a
Asheesh Kumar <asheesh220720@gmail.com>
2024-06-20 16:19:27 +0530

    testing beta

--- lib/screens/screen_two.dart
+++ lib/screens/screen_two.dart
@@ -1,39 +1,39 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/route_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
-// class ScreenTwo extends StatefulWidget {
-//   const ScreenTwo({super.key});
+class ScreenTwo extends StatefulWidget {
+  const ScreenTwo({super.key});
 
-//   @override
-//   State<ScreenTwo> createState() => _ScreenTwoState();
-// }
+  @override
+  State<ScreenTwo> createState() => _ScreenTwoState();
+}
 
-// class _ScreenTwoState extends State<ScreenTwo> {
-//   @override
-//   Widget build(context) {
-//     return SafeArea(
-//         child: Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Screen Two'),
-//         backgroundColor: Colors.blue,
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           TextButton(
-//             onPressed: () {
-//               Get.back();
-//               Get.back();
-//               //Get.toNamed('/');
-//             },
-//             child: const Center(child: Text('Go back')),
-//           ),
-//         ],
-//       ),
-//     ));
-//   }
-// }
+class _ScreenTwoState extends State<ScreenTwo> {
+  @override
+  Widget build(context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen Two'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              Get.back();
+              Get.back();
+              //Get.toNamed('/');
+            },
+            child: const Center(child: Text('Go back')),
+          ),
+        ],
+      ),
+    ));
+  }
+}
 
 /*
 import 'package:cloud_firestore/cloud_firestore.dart';
